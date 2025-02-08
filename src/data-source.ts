b82037bed8c84eec1fd5611a2189e5adafc350f1
Federico Pedraza <federico.pedraza4@gmail.com>
2025-02-08 13:23:02 -0300

    added migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,18 +1,29 @@
-// src/data-source.ts
-import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import { join } from 'path';
+import { config } from 'dotenv';
+
+config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
+});
+
 import { User } from './entities';
+import { Course } from './entities/course.entity';
+import { CoursePrice } from './entities/course-price.entity';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'squid',
-  password: 'squid',
-  database: 'theenglishcrab',
-  synchronize: true,
+  host: process.env.SQL_HOST,
+  port: parseInt(process.env.SQL_PORT || '3306', 10),
+  username: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DATABASE,
+  synchronize: false,
   logging: false,
-  entities: [User],
-  migrations: [],
-  subscribers: [],
+  entities: [User, Course, CoursePrice],
+  migrations: [join(__dirname, '/migrations/*{.ts,.js}')],
+  extra: {
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  },
 });
